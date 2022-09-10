c4850a898ae53a96a239a1260d32c1506dcc7dc8
Kislitsina-Maria <ma.kis@mail.ru>
2022-09-10 21:03:53 +0300

    Added first Test and yml-file

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/7iqbil5uda8kdly2/branch/main?svg=true)](https://ci.appveyor.com/project/Kislitsina-Maria/web-interface/branch/main)
---
+++ src/test/java/ru/netologe/service/TestOrderCard.java
@@ -0,0 +1,47 @@
+package ru.netologe.service;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestOrderCard {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "/Users/mashakislicina/IdeaProjects/webTest/driver/chromedriver");
+    }
+
+    @BeforeEach
+    void SetUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void ShouldTest() {
+        driver.get("http://localhost:7777");
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Мария Кислицина");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79030248197");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
